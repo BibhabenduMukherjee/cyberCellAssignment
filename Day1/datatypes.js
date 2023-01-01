da11371b599f521692f7e0f8eb4eb8bb97130927
= <mukherjee4004@gmail.com>
2023-01-01 14:43:11 +0530

    1 commit

---
+++ Day1/datatypes.js
@@ -0,0 +1,25 @@
+console.log(typeof ("hello"))
+console.log(typeof (21))
+console.log(typeof(null))
+console.log(typeof ([]))
+
+
+// 4 variables without their value
+var Name;
+var age;
+var email;
+var address;
+
+//  4 variables and their value
+Name = "Bibhabendu Mukherjee";
+age  = 21;
+email = "mukherjee4004@gmail.com";
+address = "Sonamukhi , Bankura";
+
+const person = {};
+person.firstName = Name; person.myAge = age; person.Email = email; person.Addredd = addredd;
+
+var Myage = 21;
+var yourAge = 20;
+console.log("I am " + Myage + "years old");
+console.log("You are " + yourAge + "years old");
---
+++ Day1/variable.js
@@ -0,0 +1,4 @@
+var name = "bivu";
+var age = 21;
+var is_single = true;
+var disability = null;
